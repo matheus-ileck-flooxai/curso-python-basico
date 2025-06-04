e1b6509e5f5a38a1db8802f0934901ef1d1bfd81
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-04 12:41:28 -0300

    feat: add Python file to explain about print and string

---
+++ aula2.py
@@ -0,0 +1,17 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+
+#Aspas simples
+print('Hello World!')
+#Aspas duplas
+print("Hello World!")
+
+#Escape
+print('Hello \" World!')
+
+#r
+print(r'Hello \" World!')
