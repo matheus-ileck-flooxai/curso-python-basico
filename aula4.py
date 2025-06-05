fb469f9f0c18d60bcef76899d89ce660e16394b7
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-05 09:06:38 -0300

    feat: add Python script to explain about boolean

---
+++ aula4.py
@@ -0,0 +1,8 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis:
+# Sim (true) ou não (False).
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que
+# questiona se um valor é igual ou outro
+
