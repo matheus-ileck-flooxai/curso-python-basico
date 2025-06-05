fe52bee1f62bfa68ead15668e6983dd03750a2eb
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-05 10:32:47 -0300

    feat: add Python script with formatted strings

---
+++ aula11.py
@@ -0,0 +1,8 @@
+nome = 'Matheus Ileck Farias'
+altura = 1.85
+peso = 63
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura')
+print('Pesa', peso, 'quilos e seu imc é')
+print(imc)
---
+++ aula12.py
@@ -0,0 +1,7 @@
+nome = 'Matheus Ileck Farias'
+altura = 1.85
+peso = 63
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura} de altura, Pesa {peso} Quilos e seu imc é {imc:.2f}'
+print(linha_1)
