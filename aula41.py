efd39d05d88bd5d79eae21942232caef5f0df53f
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-09 12:59:26 -0300

    feat: add list of lists example

---
+++ aula41.py
@@ -0,0 +1,13 @@
+"""
+Lista de listas e seus índices
+"""
+
+salas = [
+    ['Maria', 'Helena'],
+    ['Elaine',],
+    ['Matheus', 'Luiz','Helena', (0,10,20,30,40)],
+]
+print(salas[1][0])
+print(salas[0][1])
+print(salas[2][2])
+print(salas[2][3][2])
