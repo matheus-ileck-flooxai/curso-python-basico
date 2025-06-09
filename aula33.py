4a2dfba93087e597b9f7bce8afdb559f68b05658
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-09 10:23:59 -0300

    feat: add example of list methods

--- aula33.py
+++ aula33.py
@@ -3,17 +3,24 @@ Listas em Python
 Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
-Métodos úteis: Append, insert, pop, del, clear, extend, +
+Métodos úteis: 
+    Append = Adiciona um item ao final
+    insert = Adiciona um item no índice escolhido
+    pop = Remove do final ou do índice escolhido
+    del = apaga um índice
+    clear = limpa a lista
+    extend = estende a lista
+    + = concatena listas
 Create Read Update Delete
 Criar, ler, alterar, apagar = lista[i] (crud)
 """
 
 lista = [10,20,30,40]
 
-lista.append(50)
-lista.append(60)
-lista.append(70)
+lista.append('Matheus')
+nome = lista.pop()
+lista.append(1234)
+del lista[-1]
+lista.insert(100,5)
 
-ultimo_valor = lista.pop()
-
-print(lista, 'Removido, ', ultimo_valor)
+print(lista[4])
